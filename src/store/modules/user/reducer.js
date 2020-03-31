6fa542a47da727ad3cbc7368b672087113a10575
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-03-31 17:34:48 -0300

    Armazenando Perfil

---
+++ src/store/modules/user/reducer.js
@@ -0,0 +1,17 @@
+import produce from 'immer'
+
+const INITAL_STATE = {
+  profile: null,
+}
+
+export default function user(state = INITAL_STATE, action) {
+  switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, (draft) => {
+        draft.profile = action.payload.user
+        draft.signed = true
+      })
+    default:
+      return state
+  }
+}
