e3c57048884529ef9491e935d1f6ba5c8ab0e985
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-03-31 19:53:30 -0300

    Exibindo toasts

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects'
+import { toast } from 'react-toastify'
 
 import history from '~/services/history'
 import api from '~/services/api'
@@ -17,13 +18,14 @@ export function* signIn({ payload }) {
     const { token, user } = response.data
 
     if (!user.provider) {
-      console.tron.error('Usuário não é prestador')
+      toast.error('Usuário não é prestador')
     }
 
     yield put(signInSuccess(token, user))
 
     history.push('/dashboard')
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados')
     yield put(signFailure())
   }
 }
