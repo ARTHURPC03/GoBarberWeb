e5c0b8dd83e8ce73191819f8d6e9597faa5df142
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-03-31 17:22:09 -0300

    Autenticaçao

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,7 +1,19 @@
-const INITAL_STATE = {}
+import produce from 'immer'
+
+const INITAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+}
 
 export default function auth(state = INITAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, (draft) => {
+        draft.token = action.payload.token
+        draft.signed = true
+      })
+
     default:
       return state
   }
